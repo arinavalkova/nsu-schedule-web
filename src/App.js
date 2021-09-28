41bb869b50cad6347a58e4e0b826040fb298a1bd
Arina <sunny.qww@list.ru>
2021-09-28 22:32:27 +0700

    deleted useless stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <table border="1">
+            <tr>
+                <td>Понедельноиририк</td>
+                <td>Вторник</td>
+            </tr>
+            <tr>
+                <td>
+                    <table border="1">
+                        <tr>
+                            <td>Пара по C#</td>
+                        </tr>
+                        <tr>
+                            <td>Пара по МИМИ</td>
+                        </tr>
+                    </table>
+                </td>
+                <td>Пара по КРПО</td>
+            </tr>
+        </table>
+    );
 }
 
 export default App;

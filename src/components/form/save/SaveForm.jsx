4ecb80054319b214574043f4433c9b3c8fc9b3ab
Den4ick240 <zhigalovden@mail.ru>
2021-11-11 21:47:53 +0700

    added auto copy button for copying base64 string

--- src/components/form/save/SaveForm.jsx
+++ src/components/form/save/SaveForm.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./saveForm.css"
 
-const SaveForm = ({lessons, setVisible, next}) => {
+const SaveForm = ({ lessons, setVisible, next }) => {
+
+    const [copySuccess, setCopySuccess] = useState('');
+    const textAreaRef = useRef(null);
 
     const close = (e) => {
         e.preventDefault()
@@ -10,11 +13,37 @@ const SaveForm = ({lessons, setVisible, next}) => {
             next(true)
     }
 
+    function copyToClipboard(e) {
+        textAreaRef.current.select();
+        document.execCommand('copy');
+        // This is just personal preference.
+        // I prefer to not show the whole text area selected.
+        e.target.focus();
+        setCopySuccess('Скопировано в буфер обмена!');
+    }
+
+    let baseString = window.btoa((unescape(encodeURIComponent(JSON.stringify(lessons)))));
     return (
         <form>
             <div className="saveForm">
                 <h2 className="child">base64 строка для сохранения расписания:</h2>
-                <div className="base64String">{window.btoa((unescape(encodeURIComponent(JSON.stringify(lessons)))))}</div>
+                {/*<div className="base64String">{baseString}</div>*/}
+                <form>
+                    <textarea
+                        className="child"
+                        ref={textAreaRef}
+                        value={baseString}
+                    />
+                </form>
+                {
+                    /* Logical shortcut for only displaying the
+                       button if the copy command exists */
+                    document.queryCommandSupported('copy') &&
+                    <div>
+                        <button className="child" onClick={copyToClipboard}>Скопировать</button>
+                        {copySuccess}
+                    </div>
+                }
                 <button className="child" onClick={close}>Закрыть</button>
             </div>
         </form>

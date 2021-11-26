4a64a43a409072dad26f208eb8e7734daf9ed304
Arina <sunny.qww@list.ru>
2021-11-26 13:28:35 +0700

    added save distant button

--- src/components/form/save/SaveForm.jsx
+++ src/components/form/save/SaveForm.jsx
@@ -6,6 +6,16 @@ const SaveForm = ({ lessons, setVisible, next }) => {
 
     const [copySuccess, setCopySuccess] = useState('');
     const textAreaRef = useRef(null);
+    const [distantLink,setDistantLink] = useState('')
+
+    const saveDistant = () => {
+        const response = null // request to backend
+        if (response == null) {
+            setDistantLink('Невозможно сохранить удаленно составленное расписание')
+        } else {
+            setDistantLink('Расписание успешно сохранено\n' + response)
+        }
+    }
 
     const close = (e) => {
         e.preventDefault()
@@ -33,20 +43,27 @@ const SaveForm = ({ lessons, setVisible, next }) => {
     return (
         <form>
             <div className="saveForm">
-                <h2 className="child">base64 строка для сохранения расписания:</h2>
-                    <textarea
-                        className="child"
-                        ref={textAreaRef}
-                        value={baseString}
-                    />
-                {
-                    document.queryCommandSupported('copy') &&
-                    <div>
-                        <button className="child" onClick={copyToClipboard}>Скопировать</button>
-                        {copySuccess}
-                    </div>
-                }
-                <button className="child" onClick={saveByFile}>Сохранить файлом</button>
+                <h1 className="child">Сохранение расписания:</h1>
+                <div>
+                    <h2>Ссылка для локального сохранения:</h2>
+                     <textarea
+                         className="child"
+                         ref={textAreaRef}
+                         value={baseString}
+                     />
+                    {
+                        document.queryCommandSupported('copy') &&
+                        <div>
+                            <button onClick={copyToClipboard}>Скопировать</button>
+                            {copySuccess}
+                        </div>
+                    }
+                    <button onClick={saveByFile}>Сохранить файлом</button>
+                </div>
+                <div>
+                    <button onClick={saveDistant}>Сохранить удаленно</button>
+                    <div>{distantLink}</div>
+                </div>
                 <button className="child" onClick={close}>Закрыть</button>
             </div>
         </form>
